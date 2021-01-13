b3ec7e48d971bff71978e6dd4d9e6988b5b6e345
fumingfang <746108449@qq.com>
2021-01-13 17:33:05 +0800

    2020.1.13 concats模块

---
+++ Concats/src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @Author 扶明方
+ * @Date 2021/1/13 17:31
+ * @Version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
